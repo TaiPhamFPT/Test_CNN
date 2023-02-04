caa70adfd81cce3775a02e83601f40ca18e990f0
taithangYasuo <phaytai010899@gmail.com>
2023-02-04 15:47:03 +0700

    fix and add q10

--- cnn/Cau10.js
+++ cnn/Cau10.js
@@ -6,7 +6,7 @@ function timSoChung(arr1, arr2) {
     for (let i = 0; i < arr1.length; i++) {
         for (let j = 0; j < arr2.length; j++) {
             if (arr1[i] === arr2[j]) {
-                result.push(arr1)
+                result.push(arr1[i])
             }
         }
     }
